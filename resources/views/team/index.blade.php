b0898f895d9f58f2806cad53416cafed4f0a213f
edsonvaz <e9819@outlook.com>
2022-05-10 17:14:35 -0500

    TEAM ROUTE ACT 10

---
+++ resources/views/team/index.blade.php
@@ -0,0 +1,33 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+<h1 style='text-align:center'>
+    EQUIPO 4
+</h1>
+
+<div class="card-group">
+  <div class="card">
+    <img src="im2.jpg" class="card-img-top">
+    <div class="card-body">
+      <h5 class="card-title">Edson Vázquez Cruz</h5>
+      <p class="card-text">Mi experiencia en desarrollo web se sintetiza con experiencia en NodeJS, Express, JavaScript y utilizando herramientas como Github.</p>
+      <p class="card-text"><small class="text-muted">A01411799</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Edgar Yair Camacho Paredes</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      <p class="card-text"><small class="text-muted">A01206849</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Erick Eduardo Avalos Riveros
+</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      <p class="card-text"><small class="text-muted">A01276004</small></p>
+    </div>
+  </div>
+</div>
