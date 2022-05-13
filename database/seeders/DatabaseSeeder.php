297a4d0dae5ad8592ea0f31ad7789b5a504b41dc
Yair Camacho <edgarycp@gmail.com>
2022-05-13 08:01:04 -0500

    added basic seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,11 +14,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call([
+            AsistenteSeeder::class,
+            CategoriaSeeder::class,
+            MetodoPagoSeeder::class,
+            PermisoSeeder::class,
+            RolSeeder::class,
+            UsuarioSeeder::class
+        ]);
     }
 }
---
+++ database/seeders/PermisoSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PermisoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('permisos')->insert([
+            'id_permiso' => 1,
+            'nombre' => "Consultar donaciones"
+        ]);
+    }
+}
