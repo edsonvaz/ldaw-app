265abdfe07f4960ae9c5b1b9c75804d5e1b6f26e
Yair Camacho <edgarycp@gmail.com>
2022-06-15 16:30:39 -0500

    users seeder updated

--- database/seeders/UsuarioSeeder.php
+++ database/seeders/UsuarioSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class UsuarioSeeder extends Seeder
 {
@@ -15,14 +17,34 @@ class UsuarioSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('usuarios')->insert([
-            'id_usuario' => 1,
-            'id_rol' => 1,
-            'nombre' => "Edson",
-            'apellidos' => "Vazquez Cruz",
-            'celular' => 83365648100,
-            'correo_electronico' => "e9819@outlook.com",
-            'password' => "Wuwuuwwuwu"
+        DB::table('users')->insert([
+            [
+                'id' => 1,
+                'name' => "Edson",
+                'apellido' => "Vazquez",
+                'celular' => "8336564810",
+                'email' => "e9819@outlook.com",
+                'password' => Hash::make("helloworld"),
+                'created_at' => now()
+            ],
+            [
+                'id' => 2,
+                'name' => "Erick",
+                'apellido' => "Avalos",
+                'celular' => "4421234567",
+                'email' => "eeavalosr@gmail.com",
+                'password' => Hash::make("password"),
+                'created_at' => now()
+            ],
+            [
+                'id' => 3,
+                'name' => "Yair",
+                'apellido' => "Camacho",
+                'celular' => "4420987654",
+                'email' => "spambox.inbox@outlook.com",
+                'password' => Hash::make("thisisnotatest"),
+                'created_at' => now()
+            ]
         ]);
     }
 }
